ced8fffd99fa2723fd36c5386edaf3d458d5bdde
Mike Veen <mike-veen@hotmail.com>
2021-01-18 17:15:17 +0100

    gebruiker gegevens laten weer geven in de dashboard
    Door in de home controller de user gegevens in de user var te zetten en deze mee te geven met de view, kan ik de user gegevens op het dashboard laten zien, dit doe ik door in home.blade.php de user var te gebruiken

--- test/app/Http/Controllers/HomeController.php
+++ test/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        #Haal Data van de user op en zet in var user
+        $user = Auth::user();
+        #geef var user mee met de view door compact en dan de var
+        #nu kan je de user var gebruiken in de html met {{}}
+        return view('home', compact('user'));
     }
 }
--- test/resources/views/home.blade.php
+++ test/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header"><h3>{{ __('Dashboard') }}</h3></div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,12 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <h5>Gebruiker gegevens</h5>
+                    <ul>
+                        <li>Gebruikers ID: {{$user->id}}</li>
+                        <li>Gebruikers Naam: {{$user->name}}</li>
+                        <li>Gebruikers E-mail: {{$user->email}}</li>
+                    </ul>
                 </div>
             </div>
         </div>
