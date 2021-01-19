f2a623588d00f7541497c875746220912b86f942
Mike Veen <mike-veen@hotmail.com>
2021-01-19 15:11:48 +0100

    ChangePass view afgemaakt

--- test/resources/views/user/changePassword.blade.php
+++ test/resources/views/user/changePassword.blade.php
@@ -1,13 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-
+<!-- Check voor errors en laat zien -->
 @if (count($errors) > 0)
-    <div class="alert alert-danger"></div>
+    <div class="alert alert-danger">
         <p><strong>Oeps!</strong> Er is iets fout gegaan </p>
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{$error}}</li>
             @endforeach
         </ul>
+    </div>
 @endif
+
+{!! Form::model($user, ['method' => 'POST', 'route' => ['updatepassword', $user->id]]) !!}
+<div class="row">
+    <div class="col-lg-3">{!! Form::label('old_password', 'Oud wachtwoord: ') !!}</div>
+    <div class="col-lg-3">{!! Form::password('old_password', ['class'=>'form-control']) !!}</div>
+</div>
+<div class="row">
+    <div class="col-lg-3">{!! Form::label('password', 'Nieuw wachtwoord: ') !!}</div>
+    <div class="col-lg-3">{!! Form::password('password', ['class'=>'form-control']) !!}</div>
+</div>
+<div class="row">
+    <div class="col-lg-3">{!! Form::label('password_confirmation', 'Herhaal wachtwoord: ') !!}</div>
+    <div class="col-lg-3">{!! Form::password('password_confirmation', ['class'=>'form-control']) !!}</div>
+</div>
+<div class="row">
+    <div class="col-lg-3"></div>
+    <div class="col-lg-3"><button type="submit" class="btn btn-primary">Opslaan</button></div>
+</div>
+
+{!! Form::close() !!}
+
+@endsection
