34f275595650bc1385f55c6440d8d29f5a3a91f7
Mike Veen <mike-veen@hotmail.com>
2021-01-22 14:31:58 +0100

    Wachtwoord update pop up form mooier gemaakt

--- test/resources/views/home.blade.php
+++ test/resources/views/home.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-{{$user}}
+<!-- voor test {{$user}} -->
 
-<!--- Pop Up HTML word niet geshowed door de modal mudele van bootsrap alleen als je op de knop drukt -->
+<!--- Pop Up HTML voor het veranderen van jouw persoonlijke kleuren -->
 <div class="modal fade bd-example-modal-lg" id="colorchange_modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <form action="/cssupdate" method="post">
         @csrf
         <label for="achtergrond">Kies een achtergronds kleur:</label>
-        <input type="text" name="achtergrondkleur" id="achtergrond">
+        <input type="color" value="#ff0000" name="achtergrondkleur" id="achtergrond">
 
         <label for="tekst">Kies een tekst kleur:</label>
-        <input type="text" name="tekstkleur" id="tekst">
+        <input type="color" value="#ff0000" name="tekstkleur" id="tekst">
 
         <button type="submit" class="btn btn-warning">verander</button>
       </form>
@@ -21,22 +21,37 @@
   </div>
 </div>
 
-<!-- HIer komt de code voor de pop up waarmee je jouw wachtwoord veranderd -->
+<!-- Hier komt de code voor de pop up waarmee je jouw wachtwoord veranderd -->
 <div class="modal fade bd-example-modal-lg" id="passchange_modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
-      <form action="/passupdate" method="post">
+      <form class="form-horizontal" action="/passupdate" method="post">
         @csrf
-        <label for="og_pass">Orginele wachtwoord:</label>
-        <input type="password" name="og_pass" id="og_pass">
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="og_pass">Orginele wachtwoord:</label>
+          <div class="col-sm-10">
+            <input type="password" placeholder="Huidig wachtwoord" class="form-control" name="og_pass" id="og_pass">
+          </div>
+        </div>
 
-        <label for="new_pass">Nieuw wachtwoord:</label>
-        <input type="password" name="new_pass" id="new_pass">
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="new_pass">Nieuw wachtwoord:</label>
+          <div class="col-sm-10">
+            <input type="password" placeholder="Nieuwe wachtwoord" class="form-control" name="new_pass" id="new_pass">
+          </div>
+        </div>
 
-        <label for="confirm_pass">Herhaal wachtwoord:</label>
-        <input type="password" name="confirm_pass" id="confirm_pass">
-
-        <button type="submit" class="btn btn-warning">verander</button>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="confirm_pass">Herhaal wachtwoord:</label>
+          <div class="col-sm-10">
+            <input type="password" placeholder="Herhaal wachtwoord" class="form-control" name="confirm_pass" id="confirm_pass">
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-warning">verander</button>
+          </div>
+        </div>
       </form>
     </div>
   </div>
@@ -74,6 +89,7 @@
                     </div>
                   @endif
                 <!-- Einde message -->
+                <!-- Begin user gegevens en buttons-->
                     <h5>Gebruiker gegevens</h5>
                     <ul>
                         <li>Gebruikers ID: {{$user->id}}</li>
@@ -82,12 +98,13 @@
                         <br>
                         <!-- Pop up knop hier onder-->
                         <li><button type="button" id="pop-up-1" class="btn btn-outline-warning btn-sm" data-toggle="modal" data-target="#colorchange_modal">
-                          Verander tekst kleur
+                          Verander kleuren
                         </button></li>
                         <li><button type="button" id="pop-up-2" class="btn btn-outline-warning btn-sm" data-toggle="modal" data-target="#passchange_modal">
                           Verander wachtwoord
                         </button></li>
                     </ul>
+                <!-- Einde User Gegevens En Buttons -->
                 </div>
             </div>
         </div>
